fecef36c845564e31bdcf9c04212622a3e533743
Evgeny Myshkin <ev.myshkin@gmail.com>
2022-11-07 10:41:54 +0300

    added homework files for cmd line language parameter test

--- stepik/chapter3_language_select/add_to_cart_button_test.py
+++ stepik/chapter3_language_select/add_to_cart_button_test.py
@@ -5,7 +5,7 @@ from selenium.webdriver.common.by import By
 link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
 
 
-def test_language(browser):
+def test_add_to_cart_button(browser):
     browser.get(link)
     assert len(browser.find_elements(By.CSS_SELECTOR,
                '#add_to_basket_form .btn')) == 1, 'No "Add to cart" button'
--- stepik/chapter3_language_select/conftest.py
+++ stepik/chapter3_language_select/conftest.py
@@ -4,34 +4,36 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
-options = Options()
-
 
 def pytest_addoption(parser):
-    parser.addoption('--language', action='store', default=None,
-                     help="Choose language: fr, en or es")
+    parser.addoption('--browser_name', action='store', default='chrome',
+                     help="Choose browser: 'chrome' or 'firefox'")
+    parser.addoption('--language', action='store', default='en',
+                     help="Choose language: 'ru' or 'en' or 'es' or 'fr'")
 
 
 @pytest.fixture(scope="function")
 def browser(request):
-    language = request.config.getoption("language")
-    if language == "fr":
-        print(f"\nStarting browser for testing language {language}")
-        options.add_experimental_option(
-            'prefs', {'intl.accept_languages': f'{language}'})
+    browser_name = request.config.getoption("--browser_name")
+    user_language = request.config.getoption("--language")
+
+    options = Options()
+    options.add_experimental_option(
+        'prefs', {'intl.accept_languages': user_language})
+
+    fp = webdriver.FirefoxProfile()
+    fp.set_preference("intl.accept_languages", user_language)
+
+    browser = None
+    if browser_name == "chrome":
+        print(f"\nStarting {browser_name} browser for test")
         browser = webdriver.Chrome(options=options)
-    elif language == "en":
-        print(f"\nStarting browser for testing language {language}")
-        options.add_experimental_option(
-            'prefs', {'intl.accept_languages': f'{language}'})
-        browser = webdriver.Chrome(options=options)
-    elif language == "es":
-        print(f"\nStarting browser for testing language {language}")
-        options.add_experimental_option(
-            'prefs', {'intl.accept_languages': f'{language}'})
-        browser = webdriver.Chrome(options=options)
+    elif browser_name == "firefox":
+        print(f"\nStarting {browser_name} browser for test")
+        browser = webdriver.Firefox(firefox_profile=fp)
     else:
-        raise pytest.UsageError("--language should be fr, en or es")
+        raise pytest.UsageError(
+            "--browser_name should be 'chrome' or 'firefox'")
     yield browser
-    print("\nQuitting browser")
+    print("\nquit browser..")
     browser.quit()
